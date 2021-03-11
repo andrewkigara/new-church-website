ab50be7a009b8cc77f0a90be90cff6f893ad8c90
Andrew Kigara <andrewkigara@gmail.com>
2021-03-11 11:48:06 -0500

    a few necessary changes

--- frontend/src/Components/Pages/EventsPage/Events.js
+++ frontend/src/Components/Pages/EventsPage/Events.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import './Events.css'
+import congregation from '../../../assets/congregation.jpg';
 
 export const Events = () => {
     return (
-        <div className="container text-light">
-            <h1>Events Page</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            
+        <div className="container mx-auto mb-4">
+            <div class="row evCard bg-light position-relative mt-4">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                    <img src={congregation} class="w-100 evCard" alt="in-person congregation"/>
+                </div>
+                <div class="col-md-6 position-static p-4 pl-md-0">
+                    <h3 class="mt-0 evTitle">Worship with us In-person &#38; Online</h3>
+                    <p className="font-weight-lighter">Every Sunday at 11:00 AM</p>
+                    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                    <p>Join Us <a href="/" className="text-dark">here (map)</a></p>
+                </div>
+            </div>
+
+            <div class="row evCard bg-light mt-4">
+                <div class="col-6 my-4">
+                    <h2 class="text-right evTitle">Join us for Bible Study</h2>
+                    <p className="font-weight-lighter text-right">Every Sunday at 10:00 AM</p>
+                    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+                    <p>Join Us <a href="/" className="text-dark">here (map)</a></p>
+                </div>
+                <div class="col-6">
+                    <img src={congregation} class="w-100 my-4 evCard" alt="in-person congregation"/>
+                </div>
+            </div>
+
+            {/* <div class="row evCard bg-light mt-4">
+
+            </div> */}
+
         </div>
     )
 }
--- frontend/src/Components/Pages/Home/Home.js
+++ frontend/src/Components/Pages/Home/Home.js
@@ -11,7 +11,7 @@ export const Home = () => {
                     <br/>
                     <br/>
                     <h1 class="display-1">Welcome Home</h1>
-                    <p class="lead display-5">"My House shall be called a House of Worship for all nations"</p>
+                    <p class="lead display-5">"My House shall be called a House of Prayer for all nations"</p>
                     <br/>
                     <br/>
                 </div>
@@ -19,13 +19,14 @@ export const Home = () => {
 
             <div class="card card0 mb-3 container shadow">
                 <div class="row no-gutters">
-                    <div class="col-md-4 my-2 ">
+                    <div class="col-md-4 my-4 ">
                         <img src={worshipHands} class="card-img cardImg" alt="Worship"/>
                     </div>
                     <div class="col-md-8">
                     <div class="card-body text-center">
                         <h5 class="card-title h2">Join Our Services</h5>
                         <p class="card-text">Sunday Service | Every Sunday | 11:00AM EST</p>
+                        <p class="card-text">Bible Study | Every Sunday | 10:00AM - 10:45AM EST</p>
                         <a href="/events"><p class="card-text"><small class="text-muted"> Learn more ...</small></p></a>
                         <hr/>
                         <p class="card-text">Join Our Upcoming Events</p>
@@ -41,7 +42,7 @@ export const Home = () => {
                     <br/>
                     <br/>
                     <h1 class="h1">Our Mission</h1>
-                    <p class="display-5">"My House shall be called a House of Worship for all nations"</p>
+                    <p class="display-5">"My House shall be called a House of Prayer for all nations"</p>
                     <br />
                     <br />
                     <br />
